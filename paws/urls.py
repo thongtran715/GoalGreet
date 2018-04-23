3d59fa4ee11a3f4b3bac9b6ce008470fd8ae490f
thongtran715 <thongtran715@gmail.com>
2018-04-23 00:55:53 -0400

    Adding Comments Views

--- paws/urls.py
+++ paws/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # This is all the paws
-    
+    path('<int:paws_id>/comment',views.comment, name='comment')    
+
 ]
--- paws/views.py
+++ paws/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render ,redirect
 from django.template import loader
+from django.contrib.auth.models import User
 from . models import Paws, Follow_Up, Comments
 from django.http import HttpResponse
+from django.urls import reverse
 # Create your views here.
 def index(request):
     all_paws = Paws.objects.all()[:10]
@@ -11,3 +13,10 @@ def index(request):
         'all_comments' : all_comments,
     } 
     return render(request,'paws/index.html',context)
+
+def comment(request, paws_id):
+    user = User.objects.get(username="thongtran")
+    paws = Paws.objects.get(id=paws_id)
+    comment = Comments(fromUserId=user, toPawsId = paws, comment_content=request.POST["comment"])
+    comment.save()
+    return HttpResponse('Saved it')
